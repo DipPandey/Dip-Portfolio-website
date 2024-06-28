004167c9c52b754233b46915fd20b809fc2911f5
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-06-28 23:18:24 +1000

    new experience design

--- components/Experience.js
+++ components/Experience.js
@@ -7,7 +7,7 @@ const experiences = [
         description: 'Developed a backup manager software as part of the Omnicore project at the University of Tasmania, ensuring data security and reliability for university systems.',
         duration: 'Aug 2022 - May 2023',
         website: 'https://omnicore.com.au',
-        skills: ['html','CSS','Python', 'PHP', 'Django', 'PostgreSQL', 'AWS']
+        skills: ['HTML', 'CSS', 'Python', 'PHP', 'Django', 'PostgreSQL', 'AWS']
     },
     {
         role: 'Web Maintenance',
@@ -47,26 +47,29 @@ const Experience = () => {
     const ref = useVisibility();
     return (
         <section id="experience" ref={ref} className="animated opacity-0">
-            <div className="container mx-auto px-1 py-2 lg:py-10">
+            <div className="container mx-auto px-4 py-10">
                 <h2 className="text-4xl font-bold mb-12 text-blue-200 text-center">Experience</h2>
-                <div className="space-y-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {experiences.map((exp, index) => (
-                        <div key={index} className="bg-gray-800 bg-opacity-80 p-6 shadow-lg rounded-lg transform transition-transform duration-300 hover:scale-105">
-                            <h3 className="text-2xl font-bold mb-2 text-white-200">{exp.role} at {exp.company || exp.project}</h3>
-                            <p className="text-gray-300 mb-2">{exp.description}</p>
-                            <p className="text-gray-500 mb-4">{exp.duration}</p>
-                            {exp.skills && (
-                                <div className="mt-4">
-                                    <h4 className="text-xl font-bold text-blue-300">Skills Utilized:</h4>
-                                    <p className="text-gray-300">{exp.skills.join(', ')}</p>
-                                </div>
-                            )}
-                            {exp.website && (
-                                <a href={exp.website} target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600 transition-colors duration-200 mt-4 block">
-                                    Visit Website
-                                </a>
-                            )}
-                        </div>
+                        <a
+                            key={index}
+                            href={exp.website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="transform transition-transform duration-300 hover:scale-105"
+                        >
+                            <div className="bg-gray-800 bg-opacity-80 p-6 shadow-lg rounded-lg cursor-pointer hover:bg-gray-700 transition duration-300 ease-in-out">
+                                <h3 className="text-2xl font-bold mb-2 text-white">{exp.role} at {exp.company || exp.project}</h3>
+                                <p className="text-gray-300 mb-2">{exp.description}</p>
+                                <p className="text-gray-500 mb-4">{exp.duration}</p>
+                                {exp.skills && (
+                                    <div className="mt-4">
+                                        <h4 className="text-xl font-bold text-blue-300">Skills Utilized:</h4>
+                                        <p className="text-gray-300">{exp.skills.join(', ')}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </a>
                     ))}
                 </div>
             </div>
