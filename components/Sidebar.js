0461197324f71cce5829fc0dc9bc6c40ddc35843
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-07-08 16:43:14 +1000

    push

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,23 +6,22 @@ import { faHome, faBriefcase, faProjectDiagram, faFileAlt, faTools, faBars, faTi
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
     return (
-        <div className={`fixed top-0 left-0 h-screen w-60 bg-gradient-to-b from-gray-800 to-gray-900 text-white shadow-lg flex flex-col justify-between transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0`}>
+        <div className={`fixed top-0 left-0 h-screen w-64 bg-gradient-to-b from-gray-800 to-gray-900 text-white shadow-lg flex flex-col justify-between transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0`}>
             <div className="flex flex-col items-center mt-6">
-                <button className="lg:hidden text-white text-4xl mb-6" onClick={toggleSidebar}>
+                <button className="lg:hidden text-white text-2xl mb-6" onClick={toggleSidebar}>
                     <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
                 </button>
-                <div className="w-24 h-20 sm:w-32 sm:h-50 md:w-40 md:h-40 lg:w-48 lg:h-48 mb-5">
+                <div className="w-50 h-40 mb-5">
                     <img
                         src="/images/photo dip.png"
                         alt="Dip Pandey"
-                        className="rounded-full w-full h-full border-2 border-gray-200 shadow-lg"
+                        className="rounded-full w-full h-full border-1 border-transparent-100 shadow-lg"
                     />
                 </div>
-
-                <div className="text-center font-bold text-6xl mb-10">
+                <div className="text-center font-bold text-2xl mb-2">
                     Dip Pandey
                 </div>
-                <div className="flex space-x-10 mb-4">
+                <div className="flex space-x-4 mb-4">
                     <a href="https://github.com/DipPandey" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
                         <FontAwesomeIcon icon={faGithub} size="lg" />
                     </a>
@@ -36,7 +35,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
                         <FontAwesomeIcon icon={faTwitter} size="lg" />
                     </a>
                 </div>
-                <div className="text-center mb-7">
+                <div className="text-center mb-6">
                     <a
                         href="/Profile.pdf"
                         target="_blank"
